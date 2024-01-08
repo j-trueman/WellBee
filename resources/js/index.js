34ab01bfa61c7a0f80c26d177d1ae9c4e954d0ac
j-trueman <josh@joshtrueman.co.uk>
2024-01-08 21:33:53 +0000

    finished basic map implementation

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,7 +4,17 @@ if ($('#jqueryconnector')) {
 	console.log("JQuery Loaded Successfully");
 };
 
-let map;
+const sock = new WebSocket('ws://192.168.1.69:8080/ws');
+
+sock.addEventListener("message", async (event) => {
+	positionData = event.data.split(',');
+	console.log(`${positionData[0]},${positionData[1]}`);
+	map.setView([positionData[0], positionData[1]]);
+});
+
+function getCurrentPosition() {
+	sock.send("request");
+}
 
 var waitForEl = function (selector, callback) {
 	if (jQuery(selector).length) {
@@ -16,41 +26,37 @@ var waitForEl = function (selector, callback) {
 	}
 };
 
-const successCallback = (position) => {
-	map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 16);
-	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		maxZoom: 19,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	}).addTo(map);
-	let marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
-}
-
-const errorCallback = (error) => {
-	console.log(error);
-}
-
-waitForEl('#map', function () {
-	navigator.geolocation.getCurrentPosition(successCallback, errorCallback, { enableHighAccuracy: true, timeout: 10000, maximumAge: 0 });
+function createProgressBars() {
 	stepsBar = new ldBar('.stepsBar', {
 		"stroke": "#f5bb26",
 		"stroke-width": "10",
 		"value": "20",
 		"path": "M3.999999999999999 84.870489570875Q0 77.94228634059948 3.999999999999999 71.01408311032397L41 6.92820323027551Q45 0 53 0L127 0Q135 0 139 6.92820323027551L176 71.01408311032397Q180 77.94228634059948 176 84.870489570875L139 148.95636945092346Q135 155.88457268119896 127 155.88457268119896L53 155.88457268119896Q45 155.88457268119896 41 148.95636945092346Z"
 	});
-	document.querySelector('.stepsBar .ldBar-label').innerHTML = "350/1000<br>STEPS"
+	document.querySelector('.stepsBar .ldBar-label').innerHTML = "350/1000<br>STEPS";
 	caloriesBar = new ldBar('.caloriesBar', {
 		"stroke": "#f5bb26",
 		"stroke-width": "10",
 		"value": "60",
 		"path": "M3.999999999999999 84.870489570875Q0 77.94228634059948 3.999999999999999 71.01408311032397L41 6.92820323027551Q45 0 53 0L127 0Q135 0 139 6.92820323027551L176 71.01408311032397Q180 77.94228634059948 176 84.870489570875L139 148.95636945092346Q135 155.88457268119896 127 155.88457268119896L53 155.88457268119896Q45 155.88457268119896 41 148.95636945092346Z"
 	});
-	document.querySelector('.caloriesBar .ldBar-label').innerHTML = "467/870<br>CALORIES"
+	document.querySelector('.caloriesBar .ldBar-label').innerHTML = "467/870<br>CALORIES";
 	milesBar = new ldBar('.milesBar', {
 		"stroke": "#f5bb26",
 		"stroke-width": "10",
 		"value": "90",
 		"path": "M3.999999999999999 84.870489570875Q0 77.94228634059948 3.999999999999999 71.01408311032397L41 6.92820323027551Q45 0 53 0L127 0Q135 0 139 6.92820323027551L176 71.01408311032397Q180 77.94228634059948 176 84.870489570875L139 148.95636945092346Q135 155.88457268119896 127 155.88457268119896L53 155.88457268119896Q45 155.88457268119896 41 148.95636945092346Z"
 	});
-	document.querySelector('.milesBar .ldBar-label').innerHTML = "1.98/2.00<br>MILES"
+	document.querySelector('.milesBar .ldBar-label').innerHTML = "1.98/2.00<br>MILES";
+}
+
+waitForEl('#map', function () {
+	createProgressBars();
+	map = L.map('map').setView(["55.7594255", "-4.1497845"], 16);
+	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+	getCurrentPosition();
 })
 
